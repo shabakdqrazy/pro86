3c6e0841411aba86e6d8b28e69038b514ac22c50
shabakdqrazy <74818339+shabakdqrazy@users.noreply.github.com>
2022-08-15 12:23:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# spectagram-stage-6
+boiler plate code for stage 6 spectagram app
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBeIIlNBGlfNaXaxps0740rfbZgJYVeHUk",
+    authDomain: "storytelling-app-e7d11.firebaseapp.com",
+    projectId: "storytelling-app-e7d11",
+    storageBucket: "storytelling-app-e7d11.appspot.com",
+    messagingSenderId: "604065027619",
+    appId: "1:604065027619:web:8148e143b141064c1ac396"
+  };
